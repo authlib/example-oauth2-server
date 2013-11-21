cbc3e12a3123f4bbc9d68eb8438247357f213583
Hsiaoming Yang <me@lepture.com>
2013-11-21 15:24:38 +0800

    Implement handlers.

--- app.py
+++ app.py
@@ -214,6 +214,29 @@ def save_token(token, request, *args, **kwargs):
     return tok
 
 
+@app.route('/oauth/token')
+@oauth.token_handler
+def access_token():
+    return None
+
+
+@app.route('/oauth/authorize', methods=['GET', 'POST'])
+@oauth.authorize_handler
+def authorize(*args, **kwargs):
+    user = current_user()
+    if not user:
+        return redirect('/')
+    if request.method == 'GET':
+        client_id = kwargs.get('client_id')
+        client = Client.query.filter_by(client_id=client_id).first()
+        kwargs['client'] = client
+        kwargs['user'] = user
+        return render_template('authorize.html', **kwargs)
+
+    confirm = request.form.get('confirm', 'no')
+    return confirm == 'yes'
+
+
 if __name__ == '__main__':
     db.create_all()
     app.run()
