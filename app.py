d1e3b6de1982894b97a719b04d9f0161e5739074
Hsiaoming Yang <me@lepture.com>
2013-11-21 11:46:34 +0800

    Setup basic server.

---
+++ app.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+
+from flask import Flask
+from flask import session, request
+from flask import render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__, template_folder='templates')
+app.debug = True
+app.secret_key = 'secret'
+app.config.update({
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
+})
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True)
+
+
+def current_user():
+    if 'id' in session:
+        uid = session['id']
+        return User.query.get(uid)
+    return None
+
+
+@app.route('/', methods=('GET', 'POST'))
+def home():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            user = User(username=username)
+            db.session.add(user)
+            db.session.commit()
+        session['id'] = user.id
+        return redirect('/')
+    user = current_user()
+    return render_template('home.html', user=user)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run()
