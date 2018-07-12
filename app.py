4f2f48cc3e74b631d9c4c3bdf8558da3de7365a2
Hsiaoming Yang <me@lepture.com>
2018-07-12 20:39:29 +0800

    Enable refresh token generator

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from website.app import create_app
 
 app = create_app({
     'SECRET_KEY': 'secret',
+    'OAUTH2_REFRESH_TOKEN_GENERATOR': True,
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
     'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
 })
