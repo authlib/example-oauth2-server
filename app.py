b24d5b053da97358b62a143be30771fd1c066467
Riley <riley@marketdesigners.com>
2019-07-01 16:58:42 +0900

    Remove cli method

--- app.py
+++ app.py
@@ -7,9 +7,3 @@ app = create_app({
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
     'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
 })
-
-
-@app.cli.command()
-def initdb():
-    from website.models import db
-    db.create_all()
